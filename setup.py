5dc6c246080a96f21ca1b36eb036498acb2f9acb
nggit <12218311+nggit@users.noreply.github.com>
2024-07-10 09:27:28 +0700

    some changes

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='asyncutor',
-    version='0.0.0',
+    version='0.0.1',
     license='MIT',
     author='nggit',
     author_email='contact@anggit.com',
